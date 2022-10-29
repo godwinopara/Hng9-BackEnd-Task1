02b4e52b1f201192435e375dab286e61fb6413d3
Godwin Opara <godwinopara62@yahoo.com>
2022-10-30 00:56:53 +0100

    completed hng9 task 1 backend task

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    slackUsername: "@godwin",
+    backend: true,
+    age: 30,
+    bio: "I am a ambition and passionate software developer. I thrive on challenges and constantly set goals for myself and I'm always looking for an opportunity to do better and achieve greatness",
+  });
+});
+
+app.listen(8000);
