6a0a3874a73990dd84c15ef69f0141e9f57b7bce
Godwin Opara <godwinopara62@yahoo.com>
2022-11-03 11:33:20 +0100

    added comment to the code base and also update the post to request to be able to accept random words as operation_type

--- Controllers/arithmetic.js
+++ Controllers/arithmetic.js
@@ -7,13 +7,41 @@ const {
 const apiCalculator = (req, res) => {
   const { operation_type, x, y } = req.body;
 
+  // Convert the operation_type(String) into an array
+  // So we can easily search for arithmetic words(e.g Addition, Substration e.t.c) from the array
+  const formatedOperation_type = operation_type.split(" ");
+
   let operationResult;
 
-  if (operation_type === "addition") {
+  // Check to see operation_type(String) that was converted to array
+  // has an arithmetic words("add" or "addition")
+  if (
+    formatedOperation_type.includes("add") ||
+    formatedOperation_type.includes("addition")
+  ) {
+    // If it contains addition or add then we can add the values
+    // sent along side the request
     operationResult = addition(x, y);
-  } else if (operation_type === "subtraction") {
+
+    // Check to see operation_type(String) that was converted to array
+    // has an arithmetic words("subtract", "subtraction" or "minus")
+  } else if (
+    formatedOperation_type.includes("subtract") ||
+    formatedOperation_type.includes("subtraction") ||
+    formatedOperation_type.includes("minus")
+  ) {
+    // If it contains "subtract","subtraction" or "minus" then we can subtract the values
+    // sent along side the request
     operationResult = subtraction(x, y);
-  } else if (operation_type === "multiplication") {
+
+    // Check to see operation_type(String) that was converted to array
+    // has an arithmetic words("multiply" or "multiplication")
+  } else if (
+    formatedOperation_type.includes("multiply") ||
+    formatedOperation_type.includes("multiplication")
+  ) {
+    // If it contains multiply or multiplication then we can multiply the values
+    // sent along side the request
     operationResult = multiplication(x, y);
   }
 
@@ -21,6 +49,7 @@ const apiCalculator = (req, res) => {
     slackUsername: "@godwinopara",
     result: operationResult,
     operation_type,
+    formatedOperation_type,
   });
 };
 
