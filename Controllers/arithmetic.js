b443f89bb74545ecc9a69f9ff5399153f270175a
Godwin Opara <godwinopara62@yahoo.com>
2022-11-04 12:43:31 +0100

    refactor: refactorjson response

--- Controllers/arithmetic.js
+++ Controllers/arithmetic.js
@@ -48,7 +48,7 @@ const apiCalculator = (req, res) => {
     operator = "multiplication";
   }
 
-  res.status(201).json({
+  res.status(201).send({
     slackUsername: "@godwinopara",
     result: Number(operationResult),
     operation_type: operator,
